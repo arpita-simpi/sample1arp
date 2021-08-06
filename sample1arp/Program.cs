5b7e2f9fd659c25ca94aa3677e157c1ce1c08c8b
arpita.simpi@gmail.com <arpita.simpi@gmail.com>
2021-08-06 12:31:11 +0530

    Add project files.

---
+++ sample1arp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample1arp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("have a nice day");
+            Console.WriteLine("bsc");
+            Console.ReadLine();
+        }
+    }
+}
